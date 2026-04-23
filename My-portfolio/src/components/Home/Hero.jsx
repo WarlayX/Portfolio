aab4ef4959700ba5752e8fbbc3fbab6a1aa9ef21
WarlayX <adewoleodutayo@gmail.com>
2026-04-23 22:30:30 +0100

    Hero-section

--- My-portfolio/src/components/Home/Hero.jsx
+++ My-portfolio/src/components/Home/Hero.jsx
@@ -1,6 +1,26 @@
+import HeroImg from '../../assets/profilesetup.png';
+
 export default function Hero () {
   return(
     <section className='pt-5'>
+
+      <div>
+        <img src={HeroImg} alt="hero" className='' />
+      </div>
+
+      <div>
+        <h1><span>DIGITAL</span> FRONTIERS.</h1>
+
+        <p>Specializing in high-performance React JS ecosystems also using HTML, CSS, and JavaScript to bridge the gap between complex logic and elegant user interface design.</p>
+
+        <div>
+          <button className=''>
+            Hire Me
+          </button>
+        </div>
+
+      </div>
+
     </section>  
   );
 }
