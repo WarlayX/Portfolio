2b1d8b9f981c2db4c1d4756ebe31fbdb1a6872e7
WarlayX <adewoleodutayo@gmail.com>
2026-04-26 16:00:42 +0100

    fix-import-paths

--- My-portfolio/src/Pages/Home.jsx
+++ My-portfolio/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
-import Hero from "../../src/components/Home/Hero";
-import About from "../../src/components/Home/About";
-import Skill from "../../src/components/Home/Skill";
-import Projects from "../../src/components/Home/Projects";
-import Contact from "../../src/components/Home/Contact";
+import Hero from "../components/Home/Hero";
+import About from "../components/Home/About";
+import Skill from "../components/Home/Skill";
+import Projects from "../components/Home/Projects";
+import Contact from "../components/Home/Contact";
 
 export default function Home() {
   return (
