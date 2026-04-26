7b2ef6a8ffa4384e5ac58f9e6fd29a012e0a8d63
WarlayX <adewoleodutayo@gmail.com>
2026-04-26 01:22:44 +0100

    hero-animation-button-text-correction

--- My-portfolio/src/components/Home/Hero.jsx
+++ My-portfolio/src/components/Home/Hero.jsx
@@ -69,11 +69,11 @@ useEffect(() => {
   return(
     <section id='about' className='pt-5'>
       <div className='flex flex-col gap-6 sm:flex-row sm:max-w-325 sm:mx-auto sm:h-110 sm:items-center lg:h-150 '>     
-        <div className='sm:w-[30%] sm:self-start'>
-          <img 
-            src={HeroImg} 
-            alt="hero" 
-            className='object-cover w-30 h-30 rounded-[10px] sm:w-50 sm:h-50 lg:w-65 lg:h-65'
+        <div className='sm:w-[30%] sm:self-start' data-aos="zoom-in" data-aos-duration="800">
+          <img
+            src={HeroImg}
+            alt="hero"
+            className='object-cover w-30 h-30 rounded-[10px] sm:w-50 sm:h-50 lg:w-65 lg:h-65 animate-bounce-slow hover:scale-105 hover:rotate-3 transition-all duration-500 cursor-pointer'
           />
         </div>
         <div className='flex flex-col gap-6 p-2.5 sm:w-[70%] sm:gap-10'>
@@ -92,7 +92,7 @@ useEffect(() => {
 
           <div className='flex'>
             <button 
-              className='bg-(--color-primary) text-(--color-base) px-6 py-2 cursor-pointer rounded-[5px] border hover:bg-transparent hover:text-(--color-primary) transition-colors duration-200'>
+              className='bg-(--color-primary) text-(--color-base) px-6 py-2 cursor-pointer rounded-[5px] border hover:bg-transparent hover:text-(--color-primary) transition-colors duration-200 text-[12px] sm:text-[15px]'>
               Hire Me
             </button>
 
@@ -101,7 +101,7 @@ useEffect(() => {
               download="Adewole-Resume.pdf" 
               target="_blank" 
               rel="noopener noreferrer"
-              className='flex items-center gap-2 bg-transparent text-(--color-primary) px-6 py-2.5 cursor-pointer rounded-[5px] border border-(--color-primary) hover:bg-(--color-primary) hover:text-(--color-base) transition-colors duration-200 ml-4'>             
+              className='flex items-center gap-2 bg-transparent text-(--color-primary) px-6 py-2.5 cursor-pointer rounded-[5px] border border-(--color-primary) hover:bg-(--color-primary) hover:text-(--color-base) transition-colors duration-200 ml-4 text-[12px] sm:text-[15px]'>             
               Download Resume 
               <i className="bx bx-file-blank text-[18px]" />
             </a>
