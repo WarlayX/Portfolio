84a71f8f58533e1284f86a6921566d292cbf5b08
WarlayX <adewoleodutayo@gmail.com>
2026-06-10 21:13:05 +0100

    brand-presentation-slides

--- My-portfolio/src/components/Designs/Projects.jsx
+++ My-portfolio/src/components/Designs/Projects.jsx
@@ -4,6 +4,14 @@ import Design2 from '../../assets/designs/design2.jpg';
 import Design3 from '../../assets/designs/design3.jpg';
 import Design4 from '../../assets/designs/design4.jpg';
 import Design5 from '../../assets/designs/design5.jpg';
+import Pre1 from '../../assets/designs/pre1.jpg';
+import Pre2 from '../../assets/designs/pre2.jpg';
+import Pre3 from '../../assets/designs/pre3.jpg';
+import Pre4 from '../../assets/designs/pre4.jpg';
+import Pre5 from '../../assets/designs/pre5.jpg';
+import Pre6 from '../../assets/designs/pre6.jpg';
+import Pre7 from '../../assets/designs/pre7.jpg';
+import Pre8 from '../../assets/designs/pre8.jpg';
 
 const designs = [
   { img: Design1, title: "Design 1" },
@@ -11,6 +19,14 @@ const designs = [
   { img: Design3, title: "Design 3" },
   { img: Design4, title: "Design 4" },
   { img: Design5, title: "Design 5" },
+  { img: Pre1, title: "Presentation 1" },
+  { img: Pre2, title: "Presentation 2" },
+  { img: Pre3, title: "Presentation 3" },
+  { img: Pre4, title: "Presentation 4" },
+  { img: Pre5, title: "Presentation 5" },
+  { img: Pre6, title: "Presentation 6" },
+  { img: Pre7, title: "Presentation 7" },
+  { img: Pre8, title: "Presentation 8" },
 ];
 
 export default function DesignsProjects() {
