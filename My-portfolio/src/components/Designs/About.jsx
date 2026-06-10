f1e51eccdc9f5f23c9c70a7c768132462484e3e5
WarlayX <adewoleodutayo@gmail.com>
2026-06-10 01:20:08 +0100

    About-design-component

---
+++ My-portfolio/src/components/Designs/About.jsx
@@ -0,0 +1,11 @@
+export default function DesignsAbout() {
+  return (
+    <section id="about" className="pt-5">
+      <div className="max-w-[1400px] mx-auto px-2.5 md:px-8">
+
+
+
+      </div>
+    </section>
+  )
+}
